38f342aa3b116daf99e1fd4b1a2678eb389b61b2
ERP Developer <erp@mimo.local>
2026-06-19 11:13:54 +0800

    feat: add i18n setup with zh/en/es translations

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
 import App from './App';
+import './i18n';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
